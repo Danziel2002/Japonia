c5b82f390bd9e21ecf52f361a680cf61e9db9001
Danziel2002 <gruescudaniel006@gmail.com>
2019-04-26 12:15:24 +0300

    Branch master

---
+++ js/size.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$('html').width($(window).width());
+	$('html').height($(window).height());
+	
+	$(window).resize(function(){
+		
+		$('html').width($(window).width());
+		$('html').height($(window).height());
+
+	});
+});
